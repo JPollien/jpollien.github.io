cb74199529b04103e958239ef8789e640555edbb
jacobtp <jpollien@gmail.com>
2025-07-09 16:19:37 -0400

    Homepage update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import './Home.css';
-import maineLandscape from '../assets/maine-landscape-drone-1.png'
+import logoText from '../assets/Logo.png'
 
 function Home() {
   return (
     <section className="hero-section">
       <div className="hero-container">
+        <div className="hero-image-wrapper">
+          <img src={logoText} alt="Downeast Landscape Imaging" className="hero-image" />
+        </div>
         <div className="hero-text">
-          <h1>Downeast Landscape Imaging</h1>
           <h2>Maine-based Photogrammetry &amp; Mapping</h2>
           <p>
-          Downeast Landscape Imaging turns Maine’s rugged coastlines and landscapes into crisp 3D data, giving builders, planners, and researchers the insights they need, fast. Elevate your project with precise drone photogrammetry and mapping you can trust.
-        </p>
+            Downeast Landscape Imaging turns Maine’s rugged coastlines and landscapes into crisp 3D data, giving builders, planners, foresters, and researchers the insights they need, fast. Elevate your project with precise drone photogrammetry and mapping you can trust.
+          </p>
           <button className="cta-button">Contact Us</button>
-        </div>
-        <div className="hero-image-wrapper">
-          <img src={maineLandscape} alt="Maine landscape" className="hero-image" />
         </div>
       </div>
     </section>
